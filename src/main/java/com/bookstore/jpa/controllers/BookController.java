48b3595589e171e28692b557ac8d739cca6b4dee
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2025-01-07 21:59:20 -0300

    final

--- src/main/java/com/bookstore/jpa/controllers/BookController.java
+++ src/main/java/com/bookstore/jpa/controllers/BookController.java
@@ -5,10 +5,10 @@ import com.bookstore.jpa.models.BookModel;
 import com.bookstore.jpa.services.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/bookstore/books")
@@ -20,8 +20,19 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<BookModel>> getAllBooks() {
+        return ResponseEntity.status(HttpStatus.OK).body(bookService.getAllBooks());
+    }
+
     @PostMapping
     public ResponseEntity<BookModel> saveBook(@RequestBody BookRecordDto bookRecordDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(bookService.saveBook(bookRecordDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable UUID id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Book deleted successfully.");
     }
 }
